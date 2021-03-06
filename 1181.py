656277f2c8cdfacde901e07aef3a9fa063993184
hwanseung2 <wazs555@hufs.ac.kr>
2021-03-06 17:44:16 +0900

    solve

---
+++ 1181.py
@@ -0,0 +1,19 @@
+N = int(input())
+if N >= 1 and N<=20000:
+    list_ = []
+    for n in range(N):
+        string = input()
+        string_count = len(string)
+        list_.append((string,string_count))
+    print(list_)
+    list_ = list(set(list_))
+    new_list=[]
+    for word, word_cnt in list_:
+        new_list.append((word_cnt, word))
+    new_list.sort()
+    for i in new_list:
+        print(i[1])
+
+ 
+
+
